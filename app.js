3675ff2718dffc034cbcd1df410e9adab3ef5453
icaro_albar <icaro.albar@live.com>
2022-09-15 17:16:00 -0300

    Configurado para o servidor

--- app.js
+++ app.js
@@ -4,33 +4,33 @@ const app = express()
 const nodemailer = require("nodemailer");
 require('dotenv').config()
 
-const enviar = _ => {
-  let transporter = nodemailer.createTransport({
-    host: process.env.AWS_SES_HOST,
-    port: 465,
-    secure: true, // true for 465, false for other ports
-    auth: {
-      user: process.env.AWS_SES_USER, // generated ethereal user
-      pass: process.env.AWS_SES_PASS, // generated ethereal password
-    },
-  });
+let transporter = nodemailer.createTransport({
+  host: process.env.AWS_SES_HOST,
+  port: 465,
+  secure: true, // true for 465, false for other ports
+  auth: {
+    user: process.env.AWS_SES_USER, // generated ethereal user
+    pass: process.env.AWS_SES_PASS, // generated ethereal password
+  },
+});
 
+app.get('/send', (req, res) => {
   transporter.sendMail({
     from: '"Fred Foo 👻" <tecnologia@hpcap.com.br>', // sender address
     to: "icaro.albar@hpcap.com.br", // list of receivers
     subject: "Hello ✔", // Subject line
     text: "Hello world?", // plain text body
-    html: "<b>Teste no servidor</b>", // html body
-  }).then(message => console.log(message))
+    html: "<b>Teste no servidor VIII</b>", // html body
+  }).then(message => {
+    console.log(message)
+    console.log('E-MAIL ENVIADO!')
+    res.send('e-mail enviado!')
+  })
     .catch(err => console.log(err))
-}
-
-app.get('/', (req,res) => {
-  res.send('Icaro Albar')
 })
 
-app.get('/send', (req,res) => {
-  enviar()
+app.get('/', (req, res) => {
+  res.send('Icaro Albar III')
 })
 
 app.listen(4000, (req, res) => console.log('SERVIDOR EM FUNCIONAMENTO...'))
